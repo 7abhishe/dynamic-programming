983a00de261c2474bbd82492692d7d29f46cbf26
7abhishe <83947791+7abhishe@users.noreply.github.com>
2023-04-23 12:16:57 +0530

    Create 2sum.cpp

---
+++ 2sum.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+vector<int> twoSum(vector<int>& nums, int target) {
+    unordered_map<int, int> map;
+    for(int i=0;i<nums.size();i++){
+        int complement = target - nums[i];
+        if(map.count(complement)){
+            return {map[complement], i};
+        }
+        map[nums[i]] = i;
+    }
+    return {};
+}
+
+int main() {
+    vector<int> nums = {2, 7, 11, 15};
+    int target = 9;
+    vector<int> result = twoSum(nums, target);
+    for(int i=0;i<result.size();i++){
+        cout << result[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
